a3bc510e704d7d2f8840a21909b18f8d6efa4a9c
John McClumpha <john@activepipe.com>
2020-10-27 08:11:54 +1100

    chore: bump version number

--- PlaguedMurderers.cs
+++ PlaguedMurderers.cs
@@ -5,7 +5,7 @@ using Oxide.Core;
 
 namespace Oxide.Plugins
 {
-    [Info("Plagued Murderers", "DarkAz", "1.0.0")]
+    [Info("Plagued Murderers", "DarkAz", "1.0.1")]
     [Description("Spawn murderers with customised clothing skin permutations")]
     class PlaguedMurderers : RustPlugin
     {
